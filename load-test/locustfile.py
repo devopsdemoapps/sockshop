13041ac53907b1de51f39160e6ed5be7efdc8bf7
Vijayboopathy <vijayboopathy.e@gmail.com>
2018-03-07 12:25:07 +0530

    removed additional endpoints for locust config

--- load-test/locustfile.py
+++ load-test/locustfile.py
@@ -15,14 +15,6 @@ class WebTasks(TaskSet):
         item_id = category_item["id"]
 
         self.client.get("/")
-        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
-        self.client.get("/category.html")
-        self.client.get("/detail.html?id={}".format(item_id))
-        self.client.delete("/cart")
-        self.client.post("/cart", json={"id": item_id, "quantity": 1})
-        self.client.get("/basket.html")
-        self.client.post("/orders")
-
 
 class Web(HttpLocust):
     task_set = WebTasks
